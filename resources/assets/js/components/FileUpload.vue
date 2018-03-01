4587443bec804c096afbf693660b0f23a183b9fb
Terry Ebieto <tebieto@gmail.com>
2018-03-01 01:11:37 +0100

    i committed all

---
+++ resources/assets/js/components/FileUpload.vue
@@ -0,0 +1,54 @@
+<template>
+    <div class="container">
+        <div class="row">
+            <div class="col-md-8 col-md-offset-2">
+                <div class="panel panel-default">
+                    <div class="panel-heading">File Component</div>
+
+                    <div class="panel-body">
+                  
+					 <legend>Form Title</Legend>
+					 <div class="form-group">
+					 <label>Upload Files</label>
+					 <input type="file" class="form-control" name="" id="" placeholder="input..." @change="fieldChange"/>
+					 </div>
+					 <button type="submit" class="btn btn-primary" @click="uploadFile">submit</button>
+					
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+		data(){
+		return {
+		attachment: null,
+		form: new FormData
+		}
+		},
+		methods: {
+		fieldChange(e){
+		let selectedFile=e.target.files[0];
+		this.attachment=selectedFile;
+		},
+		uploadFile(){
+		this.form.append('pic', this.attachment);
+		const config = {headers: {'Content-Type': 'multipart/form-data'}};
+		
+		axios.post('/upload', this.form, config).then(response=>{
+		//success
+		console.log(response);
+		})
+				.catch(response=>{
+				//errors
+				});
+			}
+		},
+        mounted() {
+            console.log('Component mounted.')
+        }
+    }
+</script>
---
+++ resources/views/readme.txt
@@ -0,0 +1,13 @@
+change 1
+change 2
+change 3
+change 4
+change 5
+change 6
+change 7
+change 8
+change 9
+change 10
+change 11
+change 12
+change 13 c cfbcf 
